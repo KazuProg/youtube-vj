24a9f5b9769c39d78b8b3407ed125a62acec048d
KazuProg <91452566+KazuProg@users.noreply.github.com>
2024-11-12 23:28:34 +0900

    Chore: update code for compatibility with GitHub Pages

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const btn = document.querySelector("#open_windows");
   btn.addEventListener("click", () => {
     chrome.windows.create({
-      url: "https://kazuprog.work/release/youtube-vj/controller",
+      url: "https://kazuprog.github.io/youtube-vj/",
       type: "popup",
       width: 960,
       height: 540,
