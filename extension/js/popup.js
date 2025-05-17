7a071309c262afb20ff955801721e324009698d0
KazuProg <91452566+KazuProg@users.noreply.github.com>
2025-05-17 23:42:59 +0900

    Fix: chrome-extension for new domain

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const btn = document.querySelector("#open_windows");
   btn.addEventListener("click", () => {
     chrome.windows.create({
-      url: "https://kazuprog.github.io/youtube-vj/",
+      url: "https://youtube-vj.kazuprog.work/",
       type: "popup",
       width: 960,
       height: 540,
