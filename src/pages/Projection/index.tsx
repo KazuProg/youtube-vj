b4c269ffe5626dd0e779e98ec05b53f335085353
KazuProg <91452566+KazuProg@users.noreply.github.com>
2025-11-16 21:20:18 +0900

    style(Projection): add initialization buttons for fullscreen and window mode
    - Introduce initContainer with fullscreen and window mode buttons for user interaction
    - Implement state management to handle initialization and fullscreen request
    - Enhance user experience with hover effects on buttons and improved layout

--- src/pages/Projection/index.tsx
+++ src/pages/Projection/index.tsx
@@ -2,15 +2,42 @@ import VJPlayer from "@/components/VJPlayer";
 import { LOCAL_STORAGE_KEY } from "@/constants";
 import { useStorageSync } from "@/hooks/useStorageSync";
 import type { MixerData } from "@/types";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styles from "./index.module.css";
 
 const ProjectionPage = () => {
   const { data: mixerData } = useStorageSync<MixerData>(LOCAL_STORAGE_KEY.mixer);
+
+  const [initialized, setInitialized] = useState(false);
+
   useEffect(() => {
     document.title = "📺 VJ投影画面";
     document.body.style.backgroundColor = "#000";
   }, []);
+
+  if (!initialized) {
+    const init = (fullscreen: boolean) => {
+      if (fullscreen) {
+        document.documentElement.requestFullscreen?.();
+      }
+      setInitialized(true);
+    };
+
+    return (
+      <div className={styles.initContainer}>
+        <button type="button" className={styles.fulModeButton} onClick={() => init(true)}>
+          全画面表示
+          <br />
+          Fullscreen
+        </button>
+        <button type="button" className={styles.winModeButton} onClick={() => init(false)}>
+          ウィンドウモード
+          <br />
+          Window Mode
+        </button>
+      </div>
+    );
+  }
 
   return (
     <div
